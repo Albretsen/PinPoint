7fd3ad99f792d8b83f30dcf8f3f0b2c2e94750d1
sanderbt <sandertorvik2@gmail.com>
2025-06-07 11:46:52 +0200

    WIP :: revert some styling in groups

--- app/(protected)/(tabs)/groups.tsx
+++ app/(protected)/(tabs)/groups.tsx
@@ -15,14 +15,13 @@ export default function GroupsScreen() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
-    paddingHorizontal: 20,
   },
 });
